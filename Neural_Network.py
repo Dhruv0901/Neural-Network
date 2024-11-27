da3983cee98d620d54861d0e4027728cb14f76d3
DhruvMehanMQ <dhruvmehan0901@gmail.com>
2024-11-27 21:12:27 +0530

    Dense Layer

--- Neural_Network.py
+++ Neural_Network.py
@@ -1,4 +1,8 @@
+from nnfs.datasets import spiral_data
 import numpy as np
+from Dense_Layer import Layer_Dense
+
+
 inputs = [[1, 2, 3, 2.5],
           [2, 5, -1, 2],
           [-1.5, 2.7, 3.3, -0.8]]
@@ -12,6 +16,9 @@ weights2 = [[0.1, -0.14, 0.5],
 biases2 = [-1, 2, -0.5]
 layer1_output = np.dot(inputs, np.array(weights).T) + biases # np.dot performs a dot product between two vectors and then np allows vector addition
 layer2_output = np.dot(layer1_output, np.array(weights2).T) + biases2
-print(layer2_output)
 
+X,y = spiral_data(100, 3)# spiral data of two points x and y stored as X and three classes 0, 1 and 2
+dense1 = Layer_Dense(2,3)# two inputs(x and y of the points) and three neurons
+dense1.forward(X)# values stored in X passed so a list with two elements are multiplied with the 3 neurons
+print(dense1.output[:7])
 
