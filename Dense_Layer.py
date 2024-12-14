1516a20e95aa53dc3d9f1928353ab83a2dbdb57e
DhruvMehanMQ <dhruvmehan0901@gmail.com>
2024-12-14 16:09:35 +0530

    Backward Propagation

--- Dense_Layer.py
+++ Dense_Layer.py
@@ -7,5 +7,10 @@ class Layer_Dense:
         self.biases = np.zeros((1, n_neurons))
 
     def forward(self, inputs):
+        self.inputs = inputs
         self.output = np.dot(inputs, self.weights) + self.biases
 
+    def backward(self, dvalues):
+        self.dinputs = np.dot(dvalues, self.weights.T)
+        self.dweights = np.dot(self.inputs.T, dvalues)
+        self.dbiases = np.sum(dvalues, axis=0, keepdims=True)
--- Neural_Network.py
+++ Neural_Network.py
@@ -5,17 +5,18 @@ from ReLU_Activation import Activation_ReLU
 from Softmax_Activation import Activation_Softmax
 from Loss_CategoricalCrossentropy import Loss_CategoricalCrossentropy
 
-inputs = [[1, 2, 3, 2.5],
+inputs = np.array([[1, 2, 3, 2.5],
           [2, 5, -1, 2],
-          [-1.5, 2.7, 3.3, -0.8]]
-weights = [[0.2, 0.8, -0.5, 1.0],
+          [-1.5, 2.7, 3.3, -0.8]])
+weights = np.array([[0.2, 0.8, -0.5, 1.0],
            [0.5, -0.91, 0.26, -0.5],
-           [-0.26, -0.27, 0.17, 0.87]]
+           [-0.26, -0.27, 0.17, 0.87]]).T
 biases = [2, 3, 0.5]
 weights2 = [[0.1, -0.14, 0.5],
             [-0.5, 0.12, -0.33],
             [-0.44, 0.73, -0.13]]
 biases2 = [-1, 2, -0.5]
+
 layer1_output = np.dot(inputs, np.array(weights).T) + biases # np.dot performs a dot product between two vectors and then np allows vector addition
 layer2_output = np.dot(layer1_output, np.array(weights2).T) + biases2
 
