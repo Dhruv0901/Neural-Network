2ddb604d827fc661e7502a61e1f146e4c5547aa8
DhruvMehanMQ <dhruvmehan0901@gmail.com>
2024-12-21 13:28:07 +0530

    Learning Rate Decay

--- SGD_Optimiser.py
+++ SGD_Optimiser.py
@@ -1,8 +1,21 @@
 class Optimiser_SGD():
 
-    def __init__(self, learning_rate=1.0):
+    def __init__(self, learning_rate=1.0, decay=0.):
         self.learning_rate = learning_rate
+        self.current_learning_rate = learning_rate
+        self.decay = decay
+        self.iterations = 0
+
+    def pre_update_params(self):# step learning decay
+        if self.decay:
+            self.current_learning_rate = self.learning_rate * (1 / (1. + self.decay * self.iterations))
 
     def update_params(self, layer):
         layer.weights += -self.learning_rate * layer.dweights
         layer.biases += -self.learning_rate * layer.dbiases
+
+    def post_update(self):
+        self.iterations += 1
+
+    # The role of an optimiser is to find the global minimum of a loss function f(x) with respect to the
+    # variable x of the lost function if taken unilaterally
