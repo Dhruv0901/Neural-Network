2bf58350a9d69a3a061ebe6f5ba051d03f8516da
DhruvMehanMQ <dhruvmehan0901@gmail.com>
2025-01-01 16:30:23 +0530

    lambda of regularisation loss l1 and l2

--- Dense_Layer.py
+++ Dense_Layer.py
@@ -2,9 +2,16 @@ import numpy as np
 
 
 class Layer_Dense:
-    def __init__(self, n_inputs, n_neurons):
+    def __init__(self, n_inputs, n_neurons,
+                 weight_regualriser_l1=0, weight_regulariser_l2=0,
+                 bias_regulariser_l1=0, bias_regulariser_l2=0):
         self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
         self.biases = np.zeros((1, n_neurons))
+        # initial lambda values
+        self.weight_regulariser_l1 = weight_regualriser_l1
+        self.weight_regulariser_l2 = weight_regulariser_l2
+        self.bias_regulariser_l1 = bias_regulariser_l1
+        self.bias_regulariser_l2 = bias_regulariser_l2
 
     def forward(self, inputs):
         self.inputs = inputs
