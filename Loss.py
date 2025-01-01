f1d18955ee5a1fcff8aa6f99f40f4affb0f980fd
DhruvMehanMQ <dhruvmehan0901@gmail.com>
2025-01-01 16:47:16 +0530

    regularisation loss l1 and l2 forward pass

--- Loss.py
+++ Loss.py
@@ -8,3 +8,18 @@ class Loss:
         sample_losses = self.forward(output, y)
         data_loss = np.mean(sample_losses)
         return data_loss
+
+    def regularisation(self, layer):
+
+        regularisation_loss = 0
+
+        if self.weight_regulariser_l1 > 0:
+            regularisation_loss += layer.weight_regulariser_l1 * np.sum(np.abs(layer.weights))
+        if self.weight_regulariser_l2 > 0:
+            regularisation_loss += layer.weight_regulariser_l2 * np.sum(layer.weights * layer.weights)
+        if self.bias_regulariser_l1 > 0:
+            regularisation_loss += layer.bias_regulariser_l1 * np.sum(np.abs(layer.weights))
+        if self.bias_regulariser_l2 > 0:
+            regularisation_loss += layer.bias_regulariser_l2 * np.sum(layer.bias * layer.bias)
+
+        return regularisation_loss
