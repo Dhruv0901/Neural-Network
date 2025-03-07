b9e1c8533e9067d350261bfd97f0e437cc1bda38
DhruvMehanMQ <dhruvmehan0901@gmail.com>
2025-03-07 15:45:58 +1100

    Mean Square Error Loss

---
+++ MeanSquaredError_Loss.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+import Loss
+
+class Loss_MeanSqauredError(Loss):
+
+    def forward(self, y_true, y_pred):
+        sample_losses = np.mean((y_true - y_pred)**2, axis=-1)# average of square of difference
+                                                    # between the actual value and the predicted value
+        return sample_losses
+
+    def backward(self, dvalues, y_true):
+        samples = len(dvalues)
+        outputs = len(dvalues[0])
+
+        self.dinputs = -2 * (y_true - dvalues) / outputs # derivative of mean square error loss
+        self.dinputs = self.dinputs / samples # this is done for the normalisation
